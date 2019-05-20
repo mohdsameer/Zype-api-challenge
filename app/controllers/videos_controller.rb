5105d46f204dc6e7a6d55847e00bed4844712a90
mohdsameer <mohdsameer1310@gmail.com>
2019-05-20 11:16:23 +0530

    Generated view for videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,9 +1,7 @@
 class VideosController < ApplicationController
-
   def index
-  	@videos =  "https://api.zype.com/videos?app_key=XWny5j0V89yb1uZu6SI_D95EADV5FzBYldE9Ny0_q0GOzcWLiruPyhN-f2Pcyohf"
-  	@response = HTTParty.get(@videos)
-    @res = JSON.parse(@response.body)
-    @res = @res["response"]
+  	details =  "https://api.zype.com/videos?app_key=XWny5j0V89yb1uZu6SI_D95EADV5FzBYldE9Ny0_q0GOzcWLiruPyhN-f2Pcyohf"
+  	http_response = HTTParty.get(details)
+    @response = http_response["response"]
   end
 end
