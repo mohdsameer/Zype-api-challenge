36aae081c901e5d9c7113f93b160af648083fd8c
mohdsameer <mohdsameer1310@gmail.com>
2019-05-20 17:38:53 +0530

    Added show page for videos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require bootstrap.min
 //= require activestorage
 //= require turbolinks
 //= require_tree .
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,18 @@
 class VideosController < ApplicationController
+  before_action :get_video_details, only: [:index, :show]
+
   def index
-  	details =  "https://api.zype.com/videos?app_key=XWny5j0V89yb1uZu6SI_D95EADV5FzBYldE9Ny0_q0GOzcWLiruPyhN-f2Pcyohf"
-  	http_response = HTTParty.get(details)
+  end
+
+  def show
+    @video = @response.select{ |video| video["_id"] == params[:id] }.first
+  end
+
+  private
+
+  def get_video_details
+    details = "https://api.zype.com/videos?app_key=XWny5j0V89yb1uZu6SI_D95EADV5FzBYldE9Ny0_q0GOzcWLiruPyhN-f2Pcyohf&api_key=zS5ZuUwwhtA18kqioTyil7ExUSNcKic8Uxu0iG-bYHsNKXc7D6JONYmzYPeDwJA1"
+    http_response = HTTParty.get(details)
     @response = http_response["response"]
   end
 end
